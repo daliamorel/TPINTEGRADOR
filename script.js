578b898a633abd27bb27839a34c522491ffda375
daliamorel <dalia.morel24@gmail.com>
2023-11-04 11:53:55 -0300

    TP FINALIZADO

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
+var capturaFormulario = document.getElementById("formulario")
+capturaFormulario.addEventListener('submit',function(e){
+    e.preventDefault()
+})
+
+
+
+var precio = document.getElementById("total").textContent
+cantidad = document.getElementById("cantidad");
+//categoria = document.getElementById(categoria).value
+//console.log(categoria)
+
+/*function resumen(){
+    var total = 200 * cantidad.value
+    console.log(total)
+    document.getElementById("total").innerText = "Total a pagar: $" + Number(total)
+}
+
+
+
+
+
 var total = 0
 var precio = document.getElementById("total").textContent
-cantidad = document.getElementById("cantidad")
-categoria = document.getElementById("categoria").value
+cantidad = document.getElementById("cantidad")*/
+categoria = document.getElementById("categoria")
 
 var enviar = document.getElementById("enviar")
-enviar.addEventListener('click',calcularPrecio)
+enviar.addEventListener('click',resumen)
 
 
-function calcularPrecio(){
-    switch (categoria){
+function resumen(){
+    var total = 200 * cantidad.value
+    switch (categoria.value){
         case "Estudiante":
-    console.log(categoria)
-
-            total = (cantidad * 200) * 20 / 100
+            total = total * 20 / 100
             break;
 
         case "Trainee":
-            total = (cantidad * 200) * 50 / 100
+            total = total * 50 / 100
             break;
 
         case "Junior":
-            total = (cantidad * 200) * 85 / 100
+            total = total * 85 / 100
             break;
 
         default:
             break;
     }
-    precio = "Total a pagar: $" + total
+    document.getElementById("total").innerText = "Total a pagar: $" + Number(total)
     console.log(total)
 }
-console.log(cantidad.value)
-console.log(cantidad)
-console.log(200 * cantidad.value)
+
 
 
 
